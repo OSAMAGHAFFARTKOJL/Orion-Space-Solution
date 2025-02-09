89f90d840c00c3ed31fcc3e5bb67c9cae694d2d7
Osama Ghaffar <116808680+OSAMAGHAFFARTKOJL@users.noreply.github.com>
2025-02-09 17:51:09 +0500

    Update kafka_producer.py

--- src/kafka_producer.py
+++ src/kafka_producer.py
@@ -1,1 +1,15 @@
 
+from kafka import KafkaProducer
+import json
+import time
+import random
+
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092', 
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+while True:
+    telemetry_data = {"temperature": random.uniform(10, 30), "pressure": random.uniform(1, 5)}
+    producer.send('orion_telemetry', telemetry_data)
+    time.sleep(2)
