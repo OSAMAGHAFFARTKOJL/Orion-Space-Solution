3d612954de8f1b8b1fc64aecd02c44dbec34ee92
Osama Ghaffar <116808680+OSAMAGHAFFARTKOJL@users.noreply.github.com>
2025-02-09 17:51:24 +0500

    Update kafka_consumer.py

--- src/kafka_consumer.py
+++ src/kafka_consumer.py
@@ -1,1 +1,9 @@
 
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer('orion_telemetry', bootstrap_servers='localhost:9092', auto_offset_reset='latest')
+
+for message in consumer:
+    telemetry_data = json.loads(message.value)
+    print("Received:", telemetry_data)
